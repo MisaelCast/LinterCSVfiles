c80c1f1de4168b656014802009a20b70584bb45a
Misael Castillo Rios <144559985+MisaelCast@users.noreply.github.com>
2023-10-06 20:37:57 -0700

    Update prueba1.js

--- prueba1.js
+++ prueba1.js
@@ -1,35 +1,44 @@
+// Importa el módulo 'fs' (file system) para trabajar con el sistema de archivos.
 const fs = require('fs');
 
+// Lee el contenido del archivo 'database.txt' en formato UTF-8.
 fs.readFile('database.txt', 'utf8', (err, data) => {
+    // Manejo de errores: si ocurre un error durante la lectura del archivo, imprímelo y sal de la función.
     if (err) {
         console.error(err);
         return;
     }
 
+    // Divide el contenido del archivo en líneas y guarda la primera línea como encabezados (top).
     var cadena_split = data.split('\n');
     const top = cadena_split[0].split(',');
 
-    // Crear archivo de registro
+    // Crea un archivo de registro llamado 'validation_log.txt' para registrar los errores de validación.
     const logFileName = 'validation_log.txt';
     const logStream = fs.createWriteStream(logFileName);
 
+    // Itera sobre cada línea del archivo (comenzando desde la segunda línea, ya que la primera es de encabezados).
     for (let i = 1; i < cadena_split.length; i++) {
+        // Divide la línea actual en campos utilizando la coma como separador.
         var query = cadena_split[i].split(",");
 
+        // Validación de la cantidad de columnas y la existencia de datos en las columnas requeridas.
         if (top.length !== query.length || query.length === 0) {
             console.log(`Datos incorrectos o incompletos en el renglón ${i}`);
             logStream.write(`Error en el renglón ${i}: Número incorrecto de columnas o datos faltantes\n`);
-            continue;
+            continue;  // Salta a la próxima iteración del bucle.
         }
 
+        // Muestra en la consola la información del renglón actual.
         console.log("Renglon " + i + ":" + cadena_split[i]);
 
-        // Realizar validación de tipos de datos
+        // Realiza la validación de tipos de datos para cada campo.
         const typeValidations = ['alpha', 'numeric', 'alpha', 'alpha', 'numeric', 'numeric', 'email'];
 
         query.forEach(function(queryData, index) {
             const expectedType = typeValidations[index];
 
+            // Comprueba el tipo de dato esperado y registra errores en el archivo de registro si no coincide.
             if (expectedType === 'numeric' && !isNumeric(queryData.trim())) {
                 console.log(`Error en el renglón ${i}, columna ${index + 1}: Se esperaba un valor numérico`);
                 logStream.write(`Error en el renglón ${i}, columna ${index + 1}: Se esperaba un valor numérico\n`);
@@ -43,19 +52,26 @@ fs.readFile('database.txt', 'utf8', (err, data) => {
         });
     }
 
+    // Muestra un mensaje en la consola indicando que el proceso de validación ha terminado.
     console.log(`Proceso de validación completado. Se ha creado un archivo de registro: ${logFileName}`);
-    logStream.end();  // Cerrar el flujo de escritura del archivo de registro
+
+    // Cierra el flujo de escritura del archivo de registro.
+    logStream.end();
 });
 
 // Funciones de validación
+
+// Verifica si un valor es numérico.
 function isNumeric(value) {
     return /^\d+$/.test(value);
 }
 
+// Verifica si un valor es alfabético.
 function isAlpha(value) {
     return /^[a-zA-Z]+$/.test(value);
 }
 
+// Verifica si un valor es una dirección de correo electrónico válida.
 function isEmail(value) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
 }
